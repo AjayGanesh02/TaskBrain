986c1cce76e4ef53d1652c39836670f6b19e6a62
Ajay Ganesh <ajganesh2002@gmail.com>
2023-08-11 18:58:26 -0400

    change to get

--- src/app/message/route.ts
+++ src/app/message/route.ts
@@ -1,6 +1,6 @@
 import type { NextRequest } from "next/server";
 
-export async function POST(request: NextRequest, response: Response) {
+export async function GET(request: NextRequest, response: Response) {
   console.log(request);
   return `<Response></Response>`;
 }
