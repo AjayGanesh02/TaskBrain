5119389b69526a78a463f7fec3a33062e20a9b49
Ajay Ganesh <ajganesh2002@gmail.com>
2023-08-12 01:00:01 -0400

    restarting

--- src/app/message/route.ts
+++
@@ -1,6 +0,0 @@
-import type { NextRequest } from "next/server";
-
-export async function GET(request: NextRequest, response: Response) {
-  console.log(request);
-  return `<Response></Response>`;
-}
