7add0dc0b7329731620de0bfb2eb0f62b92cded9
Ajay Ganesh <ajganesh2002@gmail.com>
2023-08-12 21:45:52 -0400

    testing on prod

---
+++ src/pages/api/login/[code].ts
@@ -0,0 +1,60 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import { env } from "process";
+import { Client } from "@notionhq/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { code } = req.query;
+
+  const redirect =
+    env.NODE_ENV == "development"
+      ? "http://localhost:3000"
+      : "https://task-brain.vercel.app";
+
+  // Generate an access token with the code we got earlier and the client_id and client_secret we retrived earlier
+  const resp = await axios({
+    method: "POST",
+    url: "https://api.notion.com/v1/oauth/token",
+    auth: {
+      username: env.NOTION_CLIENT_ID ?? "",
+      password: env.NOTION_CLIENT_SECRET ?? "",
+    },
+    headers: { "Content-Type": "application/json" },
+    data: {
+      code,
+      grant_type: "authorization_code",
+      redirect_uri: redirect,
+    },
+  });
+
+  //   const respy = await fetch("https://api.notion.com/v1/oauth/token", {
+  //     method: "POST",
+  //     headers: {
+  //       Authorization: `${env.NOTION_CLIENT_ID}:${env.NOTION_CLIENT_SECRET}`,
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       code: code,
+  //       grant_type: "authorization_code",
+  //       redirect_uri: "http://localhost:3000",
+  //     }),
+  //   });
+
+  console.log(resp);
+
+  const notion = new Client({
+    auth: resp.data.access_token,
+  });
+
+  const data = await notion.search({
+    filter: {
+      property: "object",
+      value: "database",
+    },
+  });
+
+  res.json(data?.results || []);
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,55 @@
-import { useSession } from "next-auth/react";
+import { env } from "process";
+import { useEffect, useState } from "react";
 
-export default function Component() {
-  const { data: session, status } = useSession();
+function App() {
+  const [dbs, setdbs] = useState([]);
+  const redirect =
+    env.NODE_ENV == "development"
+      ? "http://localhost:3000"
+      : "https://task-brain.vercel.app";
 
-  if (status === "authenticated") {
-    return <p>Signed in as {session.user.email}</p>;
-  }
+  // When you open the app, this doesn't do anything, but after you sign into Notion, you'll be redirected back with a code at which point we call our backend.
+  useEffect(() => {
+    const params = new URL(window.document.location.toString()).searchParams;
+    const code = params.get("code");
+    if (!code) return;
+    fetch(`http://localhost:3000/api/login/${code}`).then(async (resp) => {
+      const final = await resp.json();
+      console.log(final);
+      setdbs(final);
+    });
+  }, []);
 
-  return <a href="/api/auth/signin">Sign in</a>;
+  return (
+    <div>
+      <a
+        style={{ display: "block" }}
+        href={`https://api.notion.com/v1/oauth/authorize?client_id=4e3bc30d-04bd-4e08-bdfe-55a0562492be&response_type=code&owner=user&redirect_uri=${redirect}`}
+      >
+        Connect to Notion
+      </a>
+      {dbs.map((db, id) => (
+        <div
+          style={{
+            display: "inline-flex",
+            whiteSpace: "pre",
+            border: "1px solid black",
+            marginBottom: 10,
+          }}
+          key={id}
+        >
+          {}
+          <form>
+            <label>phone</label>
+            <input type="tel"></input>
+            <button type="submit" name="Use this database">
+              Use This Database
+            </button>
+          </form>
+        </div>
+      ))}
+    </div>
+  );
 }
+
+export default App;
