b64d5ba833cf5d3dbb6125288a532bb55a7b46af
Ajay Ganesh <ajganesh2002@gmail.com>
2023-08-14 00:55:40 -0400

    time zones correct now

--- src/pages/api/message.ts
+++ src/pages/api/message.ts
@@ -157,7 +157,7 @@ function generatePrompt(user_info: string): string {
     The output has the following keys: 
     - task, a string 
     - category, a string that that is one of "Research", "Personal", "Work", or "School"
-    - complete_by, (optional) an ISO 8601 date and optional time
+    - complete_by, (optional) an ISO 8601 date and optional time and timezone
     ${
       user_info
         ? "The person has provided the following info to help in categorizing:" +
